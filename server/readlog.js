647c8748a9d2b67769331dc650fd3990a0f85881
Ben Cushing <ben.cushing@agilex.com>
2014-08-14 17:09:03 -0400

    Server is reading log file.

--- server/readlog.js
+++ server/readlog.js
@@ -1,26 +1,30 @@
 var http = require('http'),
     express = require('express'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
  
 var app = express();
 app.set('port', process.env.PORT || 3000); 
- 
-app.get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, 'public')));
 
-    res.send(app.getFile());
-});
+// var getFile = function() {
+//     fs.readFile('public/temp.log', 'utf8', function (err,data) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log(data);
+//         return data;
+//     });
+// }
+ 
+// app.get('/', function (req, res) {
+//     console.log(getFile());
+//     res.send({data: getFile()});
+// });
  
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
-    console.log(app.getFile());
 });
 
-app.getFile = function() {
-    fs.readFile('home/pi/temp.log', 'utf8', function (err,data) {
-        if (err) {
-            return console.log(err);
-        }
-        return data;
-    });
-}
+
 
